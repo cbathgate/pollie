d7cd0843686241a4b032a74b805ee8cbb81d05e0
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-05-13 13:44:58 -0700

    (dev) set-up server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const app = require('./server-config.js');
+const port = 8080;
+
+const server = app.listen(port);
+
+console.log('Server now listening on port ' + port);
+
+module.exports = {'server': server}
